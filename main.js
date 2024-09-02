6f9f90e37091d60d6f99ce5284019fabc3d1f0fd
Mark McDonald <mark.mcdonald@greenhouse.io>
2024-09-02 12:45:30 -0600

    Track input state in url params

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
 $(function() {
   $.fn.reverse = Array.prototype.reverse;
 
-  var xmlDoc = new Promise(function(resolve, reject) {
+  var xmlDoc = new Promise(function(resolve, _reject) {
     $.get("mbmbam-feed.xml", resolve);
   });
+
+  var $liveShowToggle = $("#live-show-toggle");
+  var $compilationToggle = $("#compilation-toggle");
+  var $skipIntroToggle = $("#skip-intro-toggle");
+  var $minimumEpisode = $("#minimum-episode");
+
+  var searchParams = new URLSearchParams(window.location.search);
+
+  if (searchParams.get("liveShows")) {
+    $liveShowToggle.prop('checked', searchParams.get("liveShows") === 'true');
+  }
+  $liveShowToggle.on('change', function() {
+    searchParams.set('liveShows', $(this).is(":checked"));
+    history.replaceState({}, "", "?" + searchParams.toString());
+  })
+
+  if (searchParams.get("compilations")) {
+    $compilationToggle.prop('checked', searchParams.get("compilations") === 'true');
+  }
+  $compilationToggle.on("change", function() {
+    searchParams.set("compilations", $(this).is(":checked"));
+    history.replaceState({}, "", "?" + searchParams.toString());
+  })
+
+  if (searchParams.get("skipIntro")) {
+    $skipIntroToggle.prop('checked', searchParams.get("skipIntro") === 'true');
+  }
+  $skipIntroToggle.on("change", function() {
+    searchParams.set("skipIntro", $(this).is(":checked"));
+    history.replaceState({}, "", "?" + searchParams.toString());
+  })
+
+  if (searchParams.get("minimumEpisode")) {
+    $minimumEpisode.val(parseInt(searchParams.get("minimumEpisode"), 10));
+  }
+  $minimumEpisode.on("change", function() {
+    searchParams.set("minimumEpisode", $(this).val());
+    history.replaceState({}, "", "?" + searchParams.toString());
+  })
 
   var episodes = xmlDoc.then(function(doc) {
     var rss = doc.documentElement;
@@ -43,17 +82,12 @@ $(function() {
   var audio = document.getElementById("audio-player");
   var source = document.getElementById("audio-source");
 
-
   episodes.then(function(episodes) {
     function loadNextEpisode() {
-      var $liveToggle = $("#live-show-toggle");
-      var $compilationToggle = $("#compilation-toggle");
-      var $skipToggle = $("#skip-intro-toggle");
-      var $minimumEpisode = $("#minimum-episode");
 
-      var live = $liveToggle.is(":checked");
+      var live = $liveShowToggle.is(":checked");
       var compilation = $compilationToggle.is(":checked");
-      var skip_intro = $skipToggle.is(":checked");
+      var skip_intro = $skipIntroToggle.is(":checked");
       var rawMinEpisode = $minimumEpisode.val() || 1;
       var minimumEpisode = Math.min(Math.max(parseInt(rawMinEpisode, 10), 1), episodes.length);
 
